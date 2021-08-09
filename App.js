eb7d3da5a541f614979f493bda1d176aca3fdb1a
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-09 18:48:22 +0900

    Getting the Location

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Loading from './Loading.js';
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading/>;
-}
+export default class extends React.Component {
+  getLocation = async() =>{
+    const location = await Location.getCurrentPositionAsync(options);
+    console.log(location)
+  }
+  componentDidMount(){//render 되고 난 후 실행
+    this.getLocation();
+  }
+  render(){
+    return <Loading />;
+  }
+} 
