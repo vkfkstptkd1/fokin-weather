dc72c1e549b4d446bea0dbc6e9084ece297b5a5a
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-09 20:17:18 +0900

    Getting Weather API

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Alert } from "react-native";
 import Loading from './Loading.js';
 import * as Location from "expo-location";
+import axios from "axios";//fetch
+
+const WEATHER_API_KEY ="765ed73d4d92f1e27e000c6371f2f459";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude,longitude) => {
+    const { weatherdata } = await axios.get(
+      `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );//axios를 이용해 openAPI 가져옴
+    console.log( weatherdata );
+  };
   getLocation = async() =>{
-    const location = await Location.getCurrentPositionAsync(options);
-    console.log(location)
+    try { 
+      await Location.requestForegroundPermissionsAsync();//client에게 위치 정보 따와도 되는지 허가요청
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();//expo의 현재 위치 가져와주는 기능
+      this.getWeather(latitude,longitude)
+      this.setState({ isLoading: false });
+    } catch {
+      Alert.alert("Can't find you.", "We can't recommend you anything.");
+    }
   }
   componentDidMount(){//render 되고 난 후 실행
     this.getLocation();
   }
   render(){
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 } 
