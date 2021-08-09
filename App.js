42bb70bd44d3ac86a339e8ae4d31005f19fd458d
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-09 17:11:15 +0900

    Loading Screen

--- App.js
+++ App.js
@@ -1,27 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading.js';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-     <View style={styles.yellowbar}></View>
-     <View style={styles.bluebar}></View>
-    </View>
-  );
+  return <Loading/>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,//flex로 구성해야 모든 앱 간 레이아웃 유동성이 좋음
-    backgroundColor: '#fff'
-  },
-  bluebar: {
-    flex: 1,
-    backgroundColor: "blue"
-  },
-  yellowbar: {
-    flex: 1,
-    backgroundColor: "yellow"
-  }
-});
