8a055ed258bb68a9463baaaabeb6338e7d0c69ca
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-10 23:45:24 +0900

    Displaying the weather_1

--- Weather.js
+++ Weather.js
@@ -1,1 +1,14 @@
-Weather.js
+import React from "react";
+import { StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+
+export default function Weather(){
+    return <View><Text></Text></View>
+}
+
+Weather.PropTypes = {
+    temp: PropTypes.number.isRequired
+};
+const styles = StyleSheets.create({
+    
+})
