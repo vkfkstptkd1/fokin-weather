71f8744c9924715375c26ce17a58a5c48e7d2060
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-21 10:37:04 +0900

    Background Gradient

--- Loading.js
+++ Loading.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default function Loading(){
     return (
     <View style = {styles.container}>
         <Text style = {styles.text}>SWEET HOME</Text>
+        <StatusBar barStyle="dark-content" />
     </View>
     );
 }
