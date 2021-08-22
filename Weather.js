5ace0ab132a28dc44560d29bdd0a785c080620ba
vkfkstptkd1 <vkfkstptkd1@naver.com>
2021-08-22 23:47:50 +0900

    arduino_test

--- Weather.js
+++ Weather.js
@@ -5,101 +5,119 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
 const weatherOptions = {
-    Haze: {
-      iconName: "weather-hail",
-      gradient: ["#4DA0B0", "#D39D38"]
-    },
     Thunderstorm: {
-      iconName: "",
-      gradient: []
-    },
-    Drizzle: {
-      iconName: "",
-      gradient: []
-    },
-    Rain: {
-      iconName: "",
-      gradient: []
-    },
-    Snow: {
-      iconName: "",
-      gradient: []
-    },
-    Atmosphere: {
-      iconName: "",
-      gradient: []
-    },
-    Clear: {
-      iconName: "",
-      gradient: []
-    },
-    Clouds: {
-      iconName: "",
-      gradient: []
-    },
-    Haze: {
-      iconName: "",
-      gradient: []
-    },
-    Mist: {
-      iconName: "",
-      gradient: []
-    },
-    Dust: {
-      iconName: "",
-      gradient: []
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"]
+      },
+      Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+      },
+      Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "Rain",
+        subtitle: "Today is raining outside."
+      },
+      Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"]
+      },
+      Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+      },
+      Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"]
+      },
+      Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"]
+      },
+      Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+      },
+      Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+      },
+      Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "Just don't go outside."
+      }
+    };
+    
+    export default function Weather({ temp, condition }) {
+      return (
+        <LinearGradient
+          colors={weatherOptions[condition].gradient}
+          style={styles.container}
+        >
+          <StatusBar barStyle="light-content" />
+          <View style={styles.halfContainer}>
+            <MaterialCommunityIcons
+              size={96}
+              name={weatherOptions[condition].iconName}
+              color="white"
+            />
+            <Text style={styles.temp}>{temp}°</Text>
+          </View>
+          <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+            <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+            <Text style={styles.subtitle}>
+              {weatherOptions[condition].subtitle}
+            </Text>
+          </View>
+        </LinearGradient>
+      );
     }
-  };
-
-export default function Weather({ temp,condition }) {
-  return (
-    <LinearGradient
-      colors={weatherOptions[condition].gradient}
-      style={styles.container}
-    >
-      <StatusBar barStyle="light-content" />
-      <View style={styles.halfContainer}>
-        <MaterialCommunityIcons
-          size={96}
-          name={weatherOptions[condition].iconName}
-          color="white"
-        />
-        <Text style={styles.temp}>{temp}°</Text>
-      </View>
-      <View style={styles.halfContainer} />
-    </LinearGradient>
-  );
-}
-
-Weather.propTypes = {
-    temp: PropTypes.number.isRequired,
-    condition: PropTypes.oneOf([
-      "Thunderstorm",
-      "Drizzle",
-      "Rain",
-      "Snow",
-      "Atmosphere",
-      "Clear",
-      "Clouds",
-      "Haze",
-      "Mist",
-      "Dust"
-    ]).isRequired
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-    },
-    temp: {
-    fontSize: 42,
-    color: "white"
-    },
-    halfContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-    }
-});
+    Weather.propTypes = {
+      temp: PropTypes.number.isRequired,
+      condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+      ]).isRequired
+    };
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      temp: {
+        fontSize: 42,
+        color: "white"
+      },
+      halfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      title: {
+        color: "white",
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10
+      },
+      subtitle: {
+        fontWeight: "600",
+        color: "white",
+        fontSize: 24
+      },
+      textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start"
+      }
+    });
